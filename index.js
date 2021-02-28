22a4f2a6db16a90fe6dfee09fa4e223a649adcc8
S.Kiri <s.kirianov@gmail.com>
2021-02-28 23:21:31 +0200

    uibuild

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 
 
