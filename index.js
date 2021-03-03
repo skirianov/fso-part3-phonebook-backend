a49c4a6085e85e5bdffacdcbcce7e6039b2afce0
Sergii <s.kirianov@gmail.com>
2021-03-03 23:20:08 +0200

    installed airbnb eslint config

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 const cors = require('cors');
 const Person = require('./models/persons');
 
-
+// eslint-disable-next-line no-unused-vars
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());
@@ -12,97 +13,88 @@ app.use(express.static('build'));
 app.use(express.json());
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then( foundPersons => {
-        response.json(foundPersons);
-    })
-})
+  Person.find({}).then((foundPersons) => {
+    response.json(foundPersons);
+  });
+});
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then( person => {
-        if (person) {
-            response.json(person);
-        } else {
-            response.status(400).end();
-        }
-    })
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(400).end();
+    }
+  })
     .catch((error) => next(error));
-})
-
-app.get('/info', (request, response) => {
-    const length = persons.length;
-    response.send(
-        `<p>Phonebook has info for ${length} people.</p>
-         <p>${new Date()}</p>`
-    )
-})
+});
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end();
-        })
-        .catch( error => next(error));
-})
+  Person.findByIdAndRemove(request.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body;
+  const { body } = request;
 
-    if (!body.name || !body.number) {
-        response.status(400).json({
-            error: "Missing content"
-        })
-    }
+  if (!body.name || !body.number) {
+    response.status(400).json({
+      error: 'Missing content',
+    });
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
     })
-        
-    person.save()
-    .then( savedPerson => {
-        response.json(savedPerson);
-    })
-    .catch( error => next(error));
-}
-)
+    .catch((error) => next(error));
+});
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body;
+  const { body } = request;
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
-        .then( updatedPerson => {
-            response.json(updatedPerson);
-        })
-        .catch( error => next(error));
-
-})
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message);
+  console.log(error.message);
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
 
-    next(error);
-}
+  next(error);
+};
 
 app.use(errorHandler);
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: "unknown endpoint" });
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- models/persons.js
+++ models/persons.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -5,35 +7,35 @@ const url = process.env.MONGODB_URI;
 
 console.log('connecting to ', url);
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
-    .then( result => {
-        console.log('connected to MongoDB')
-    })
-    .catch( error => {
-        console.log('error connecting to MongoDB: ', error.message);
-    })
+mongoose.connect(url, {
+  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
+})
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 5,
-        required: true
-    }
-  })
-  
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+});
+
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
-})
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 module.exports = mongoose.model('Person', personSchema);
-
-
